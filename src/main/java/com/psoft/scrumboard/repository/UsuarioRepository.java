b1d064e7f7f4b2e63d22c50611d352746680426c
pedrorneto1 <pedrorneto1@gmail.com>
2022-07-31 22:42:36 -0300

    Criação do repository de usuários do sistema

---
+++ src/main/java/com/psoft/scrumboard/repository/UsuarioRepository.java
@@ -0,0 +1,27 @@
+package com.psoft.scrumboard.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.psoft.scrumboard.model.Usuario;
+
+@Repository
+public class UsuarioRepository {
+	
+	private Map<String, Usuario> usuarios;
+	
+	public UsuarioRepository() {
+		this.usuarios = new HashMap<String, Usuario>();
+	}
+	
+	public void addUser(Usuario usuario) {
+		this.usuarios.put(usuario.getUsername(), usuario);
+	}
+	
+	public boolean containsUsername(String username) {
+		return this.usuarios.containsKey(username);
+	}
+
+}
