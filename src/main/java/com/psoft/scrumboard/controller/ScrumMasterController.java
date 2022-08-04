c53a61da9c9d1074ddf3fdf56125f513b3366e0c
Karen Aciole <karen.alves@ccc.ufcg.edu.br>
2022-08-04 00:53:32 -0300

    lista de papeis disponiveis que um usuario pode ser assumido em um projeto

--- src/main/java/com/psoft/scrumboard/controller/ScrumMasterController.java
+++ src/main/java/com/psoft/scrumboard/controller/ScrumMasterController.java
@@ -1,4 +1,26 @@
 package com.psoft.scrumboard.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+
 public class ScrumMasterController {
+
+    @RequestMapping(value = "/scrummaster/papeisDisponiveis", method = RequestMethod.GET)
+    public ResponseEntity<?> listaPapeis() {
+        String papeis = "Papéis que podem ser assumidos por usuários associados a projetos:\n\n" +
+                "- Product Owner\n" +
+                "- Pesquisador\n" +
+                "- Desenvolvedor\n" +
+                "- Estagiário";
+
+        return new ResponseEntity<String>(papeis, HttpStatus.OK);
+    }
 }
